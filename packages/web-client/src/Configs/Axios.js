0070eb91171545c33c099f2318a5e291f8325633
QuocTD <quoc.tongdinh@thegioididong.com>
2021-11-15 20:33:25 +0700

    up

--- packages/web-client/src/Configs/Axios.js
+++ packages/web-client/src/Configs/Axios.js
@@ -1,10 +1,14 @@
 import axios from 'axios'
 
+
 var axiosClient = axios.create({
     baseURL: `${process.env.REACT_APP_API_URL}`,
     // `withCredentials` chỉ định có thực hiện các request cross-site Access-Control sử dụng credential hay không
     withCredentials: false, // mặc định là false,
-    headers: {'content-type': 'application/json'},
+    headers: {'content-type': 'application/json',
+            'Authorization':`Bearer ${localStorage.getItem('token')}`
+    },
+
     // `responseType` chỉ định kiểu dữ liệu mà server sẽ trả về
     // có thể là 'arraybuffer', 'blob', 'document', 'json', 'text', 'stream'
     //responseType: 'json', // default
