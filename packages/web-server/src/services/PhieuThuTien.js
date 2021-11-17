bd503321b06966e4936e9d4218bd3d677f6be242
tinhtrinh <chumeoconxauxa@gmail.com>
2021-11-17 23:04:56 +0700

    thêm tạo ds và ctds vào api phiếu thu tiền

--- packages/web-server/src/services/PhieuThuTien.js
+++ packages/web-server/src/services/PhieuThuTien.js
@@ -1,13 +1,11 @@
-const { PhieuThuTien, Xe, HieuXe, DoanhSo } = require('../models');
+const { PhieuThuTien, Xe, DoanhSo, ChiTietDoanhSo } = require('../models');
 
 /* `````````````````````````````````` */
 // Put your custom services code below this line
 exports.create = async formInput => {
-    // let month = formInput.NgayTT.getMonth();
+    let date = new Date(formInput.ngayTT);
 
     let xe = await Xe.findOne({ bienSo: formInput.bienSo }, { maHieuXe: 1, bienSo: 1, tienNo: 1 });
-    // let { id } = await Xe.findOne({ ThoiDiemBaoCao: month }, { _id: 1 });
-    // await Accessory.updateOne({ _id: formInput.accessoryId} , { remaining: newRemaining });
 
     if(!xe) {
         return Promise.reject({ message: 'Xe not found' })
@@ -25,10 +23,36 @@ exports.create = async formInput => {
 
     await newPTT.save();
 
-//     DoanhSo.aggregate({$project: {userID: 1, month: {$month: '$createdAt'}, year: { $year: '$createdAt'}}}, 
-// { $match: {month: 5, year: 2021} })
-    
-// insert({'maPTT': '1', 'NgayTT': ISODate('2021-11-11'), 'SoTienThu': 100000})
+    let ds = await DoanhSo.aggregate([{$project: { month: {$month: '$ThoiDiemDS'}, year: { $year: '$ThoiDiemDS'}, tongDS: 1}}, 
+    {$match: { month: date.getMonth() + 1, year: date.getFullYear()}}]);
+
+    if(!ds[0]){
+        let newDS = await new DoanhSo({ThoiDiemDS: formInput.ngayTT, tongDS: formInput.soTienThu});
+        await newDS.save();
+        let { _id } = await DoanhSo.findOne({ ThoiDiemDS: formInput.ngayTT }, { _id: 1 })
+        let newCtds = await new ChiTietDoanhSo({
+            maHieuXe: xe.maHieuXe,
+            tiLe: 0.5,
+            tongTien: formInput.soTienThu,
+            maDoanhSo: _id
+        });
+        await newCtds.save();
+    } else {
+        await DoanhSo.updateOne({ _id: ds[0]._id }, { tongDS: ds[0].tongDS + formInput.soTienThu });
+        let ctds = await ChiTietDoanhSo.findOne({ maDoanhSo: ds[0]._id, maHieuXe: xe.maHieuXe });
+        if(!ctds) {
+            let newCtds = await new ChiTietDoanhSo({
+                maHieuXe: xe.maHieuXe,
+                tiLe: 0.5,
+                tongTien: formInput.soTienThu,
+                maDoanhSo: ds[0]._id
+            });
+            await newCtds.save();
+        } else {
+            await ChiTietDoanhSo.updateOne({ maDoanhSo: ds[0]._id }, { tongTien: ctds.tongTien + formInput.soTienThu });
+        }      
+    }
+
     return newPTT;
 }
 
