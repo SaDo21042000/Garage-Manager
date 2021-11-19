c49b0a237828becb592ab9cd7501f32e2ca43eca
Đỗ Hải <62206572+dohaizxc@users.noreply.github.com>
2021-11-19 19:35:41 +0700

    Update Routes.jsx

--- packages/web-client/src/Routes.jsx
+++ packages/web-client/src/Routes.jsx
@@ -1,18 +1,20 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import HomePage from './Containers/HomePage';
-import CustomerPage from './Containers/CustomerPage';
+import { LookUp, TiepNhanXe, RepairPage, RepairManagement, Bill } from './CarManagement';
+import { LogIn, SignUp} from './LogIn';
 import Layout from './Components/Layout';
-import { SaleReportPage, SaleReportForm } from './Containers/SaleReportPage';
 
 function Routes() {
   return (
     <BrowserRouter>
       <Layout>
         <Switch>
-          <Route path="/" exact component={HomePage} />
-          <Route path="/customer" component={CustomerPage} />
-          <Route path="/sales-report-page" component={SaleReportPage} />
-          <Route path="/sales-report-form" component={SaleReportForm} />
+        <Route path="/look-up" component={LookUp} />
+          <Route path="/tiep-nhan-xe" component={TiepNhanXe} />
+          <Route path="/repair-page" component={RepairPage} />
+          <Route path="/repair-management" component={RepairManagement} />
+          <Route path="/bill" component={Bill} />
+          <Route path="/log-in" component={LogIn} />
+          <Route path="/sign-up" component={SignUp} />
         </Switch>
       </Layout>
     </BrowserRouter>
