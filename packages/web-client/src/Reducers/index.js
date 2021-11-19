3912f28477b8620c05a9b287e379e1e3322580a9
Đỗ Hải <62206572+dohaizxc@users.noreply.github.com>
2021-11-19 19:23:26 +0700

    Update index.js

--- packages/web-client/src/Reducers/index.js
+++ packages/web-client/src/Reducers/index.js
@@ -1,4 +1,10 @@
-import LookUp from "./LookUp";
-import TiepNhanXe from './TiepNhanXe';
-import RepairPage from './RepairPage';
-export { LookUp, TiepNhanXe, RepairPage };
+import { combineReducers } from "redux";
+import status from '../Containers/TestPage/reducers/status'
+import lstProduct from '../Containers/TestPage/reducers/lstProduct'
+
+const AppReducer=combineReducers({
+    status,
+    lstProduct
+})
+
+export default AppReducer;
